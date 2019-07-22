befb111c9f1a0d705a6a1e14426904323f969bf7
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-07-21 22:05:09 -0700

    Updates

---
+++ precache-manifest.2ecdfa8f6da18db984e99fa95ee53d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8551bd8d07235d97d410dd6a6900ce98",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "b0cd429953ccf691ccf6",
+    "url": "/monstersrolodex/static/css/main.0a62f73f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monstersrolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b0cd429953ccf691ccf6",
+    "url": "/monstersrolodex/static/js/main.8f08b08c.chunk.js"
+  },
+  {
+    "revision": "2d568aa4d8a7bd11e400",
+    "url": "/monstersrolodex/static/js/runtime~main.0a4d0c89.js"
+  }
+]);
